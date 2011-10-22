9372d9891b89b3fa9f66eb0816905f4a6c6c68b9
GWCA.ACB@gmail.com <GWCA.ACB@gmail.com@0a7abff6-15f6-fa11-c90f-40dcaecbcba0>
2011-10-22 18:07:06 +0000

    Updated Attribute Enum Added Profession Enum

---
+++ trunk/Server2011/GWLP-R/GameServer/Enums/Profession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameServer.Enums
+{
+        public enum Profession
+        {
+                None = 0,
+                Warrior,
+                Ranger,
+                Monk,
+                Necromancer,
+                Mesmer,
+                Elementalist,
+                Assassin,
+                Ritualist,
+                Paragon,
+                Dervish,
+        }
+}
