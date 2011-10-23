782de183a2985c3b51e2792d3935710a269c2b17
GWCA.ACB@gmail.com <GWCA.ACB@gmail.com@0a7abff6-15f6-fa11-c90f-40dcaecbcba0>
2011-10-23 17:50:34 +0000

    Added everything neccassery to implement Dialogues

---
+++ Server2011/GWLP-R/GameServer/Enums/ButtonIcons.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameServer.Enums
+{
+    public enum ButtonIcons
+    {
+        Assassin = 0x00,
+        Mesmer = 0x01,
+        Necromancer = 0x02,
+        Elementalist = 0x04,
+        Monk = 0x05,
+        Warrior = 0x06,
+        Ranger = 0x07,
+        Dervish = 0x08,
+        Ritualist = 0x09,
+        Paragon = 0x0A,
+
+        Link = 0x1C,
+        Cancel = 0x0F,
+        Accept = 0x10,
+        Decline = 0x11,
+    }
+}
